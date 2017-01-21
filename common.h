b0e49b7d328e28502d733506c616788f1b5e5e69
Giulio Camuffo <giuliocamuffo@gmail.com>
2017-01-21 11:43:36 +0100

    add warning and debug message types

--- common.h
+++ common.h
@@ -15,22 +15,39 @@ static void error(Args &&... args)
     exit(1);
 }
 
-struct Err
+struct Message
 {
+    enum class Type {
+        Debug,
+        Warning,
+        Error,
+    };
+
+    Type type;
     std::string filename;
     int lineno;
     int colno;
     bool firstLine;
 
-    Err(const std::string &fname, int l, int c) : filename(fname), lineno(l), colno(c), firstLine(true) {}
-    ~Err() { exit(2); }
+    Message(Type t, const std::string &fname, int l, int c) : type(t), filename(fname), lineno(l), colno(c), firstLine(true) {}
+    ~Message() { if (type == Type::Error) exit(2); }
 
     template<class... Args>
-    Err &line(Args &&... args)
+    Message &line(Args &&... args)
     {
         if (firstLine) {
             auto s = fmt::format(std::forward<Args>(args)...);
-            fmt::print(stderr, "\033[1;37m{}:{}:{}: \033[1;31merror:\033[1;37m {}\033[0m\n", filename, lineno, colno, s);
+            switch (type) {
+                case Type::Error:
+                    fmt::print(stderr, "\033[1;37m{}:{}:{}: \033[1;31merror:\033[1;37m {}\033[0m\n", filename, lineno, colno, s);
+                    break;
+                case Type::Warning:
+                    fmt::print(stderr, "\033[1;37m{}:{}:{}: \033[1;31mwarning:\033[1;37m {}\033[0m\n", filename, lineno, colno, s);
+                    break;
+                case Type::Debug:
+                    fmt::print(stderr, "\033[1;37m{}:{}:{}: \033[1;31mdebug:\033[1;37m {}\033[0m\n", filename, lineno, colno, s);
+                    break;
+            }
             firstLine = false;
         } else {
             fmt::print(stderr, std::forward<Args>(args)...);
@@ -44,13 +61,28 @@ struct Err
 template<class... Args>
 void err(const Token &tok, Args &&... args)
 {
-    Err(tok.filename(), tok.lineNo(), tok.columnNo()).line(std::forward<Args>(args)...).line("{}", tok.line()).line(std::string(std::max(tok.columnNo() - 1, 0), ' ') + '^');
+    Message(Message::Type::Error, tok.filename(), tok.lineNo(), tok.columnNo()).line(std::forward<Args>(args)...)
+                                                                               .line("{}", tok.line()).line(std::string(std::max(tok.columnNo() - 1, 0), ' ') + '^');
+}
+
+template<class... Args>
+void warning(const Token &tok, Args &&... args)
+{
+    Message(Message::Type::Warning, tok.filename(), tok.lineNo(), tok.columnNo()).line(std::forward<Args>(args)...)
+                                                                                 .line("{}", tok.line()).line(std::string(std::max(tok.columnNo() - 1, 0), ' ') + '^');
+}
+
+template<class... Args>
+void debug(const Token &tok, Args &&... args)
+{
+    Message(Message::Type::Debug, tok.filename(), tok.lineNo(), tok.columnNo()).line(std::forward<Args>(args)...)
+                                                                               .line("{}", tok.line()).line(std::string(std::max(tok.columnNo() - 1, 0), ' ') + '^');
 }
 
 template<class... Args>
 inline void error2(const std::string &filename, int lineno, int colno, Args &&... args)
 {
-    Err(filename, lineno, colno).line(std::forward<Args>(args)...);
+    Message(Message::Type::Error, filename, lineno, colno).line(std::forward<Args>(args)...);
 }
 
 #endif
